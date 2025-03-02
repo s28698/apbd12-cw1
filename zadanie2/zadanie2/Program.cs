726c047a7158f3ae04b336ac461a09f649741e0f
s28698 <s28698@pjwstk.edu.pl>
2025-03-02 09:35:49 +0100

    Modyfikacja 1

--- zadanie2/zadanie2/Program.cs
+++ zadanie2/zadanie2/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter username:");
+
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello " + userName);
