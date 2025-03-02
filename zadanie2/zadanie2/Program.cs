ee03ab3ac7f7a180fbadf26680439865ac0da8dc
s28698 <s28698@pjwstk.edu.pl>
2025-03-02 09:44:37 +0100

    Modyfikacja 3

--- zadanie2/zadanie2/Program.cs
+++ zadanie2/zadanie2/Program.cs
@@ -1,15 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
 Console.WriteLine("Enter username:");
 
 string userName = Console.ReadLine();
-
 Console.WriteLine("Hello " + userName);
 
-
 Console.WriteLine("Zweryfikuj swoją tożsamość. Ile jest 2+2 ?");
-
 string number = Console.ReadLine();
 int FinalNumber = 0;
 Int32.TryParse(number, out FinalNumber);
