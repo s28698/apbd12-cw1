3939e4ad2c0af2e105189d837001c76c5698c784
s28698 <s28698@pjwstk.edu.pl>
2025-03-02 09:43:49 +0100

    Modyfikacja 2

--- zadanie2/zadanie2/Program.cs
+++ zadanie2/zadanie2/Program.cs
@@ -6,3 +6,19 @@ Console.WriteLine("Enter username:");
 string userName = Console.ReadLine();
 
 Console.WriteLine("Hello " + userName);
+
+
+Console.WriteLine("Zweryfikuj swoją tożsamość. Ile jest 2+2 ?");
+
+string number = Console.ReadLine();
+int FinalNumber = 0;
+Int32.TryParse(number, out FinalNumber);
+
+if  (FinalNumber == 4)
+{
+    Console.WriteLine("Weryfikacja zatwierdzona.");
+}
+else
+{
+    Console.WriteLine("Weryfikacja niezatwierdzona");
+}
